91dadc7b653633af47465cc9071ac6cd7d61a48b
Stefano Pomelli <stepome@Stes-Laptop.local>
2024-10-14 17:44:15 +0800

    Frontend logic Done, Missing little design details

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,17 +3,18 @@ import { Button } from './ui/button';
 
 const Navbar = () => {
   return (
-    <nav className="p-4 bg-lighter">
+    <nav className="px-4 py-2 border-b-2" style={{ borderColor: '#1F2295' }}>
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
         <div className="text-white text-xl font-bold">
-          <Link href="/">MyApp</Link>
+          <Link href="/">BetAPT</Link>
         </div>
 
         {/* Connect Button */}
         <div>
-          <button className="bg-button text-white px-4 py-2 rounded hover:bg-blue-600">
-            Connect
+          <button className="connect-button-hover rounded-full border-4 bg-button text-white px-4 py-2 rounded text-lg" style={{ borderColor: '#4D147C' }}
+          >
+            CONNECT
           </button>
         </div>
       </div>
